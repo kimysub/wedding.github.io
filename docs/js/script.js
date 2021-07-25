decaac343dae0bb75cd9b84427bbbd83209f5401
Youngsub <kimysub@gmail.com>
2021-07-25 14:31:50 +0000

    ver8

--- docs/js/script.js
+++ docs/js/script.js
@@ -77,6 +77,10 @@
 
 $(document).ready(function() {
 
+    $('#go-to-top').click(function () {
+        $('html,body').animate({scrollTop: 0}, 400);
+        return false;
+    });
 	// 01. BROWSER AGENT FUNCTION
 	//==================================================================================
 
